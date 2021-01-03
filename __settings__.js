8eaecf619880f4dc068722e1ec6aed567b55a3bf
vpardinho <victorpardinho@gmail.com>
2021-01-03 15:18:52 +0100

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1062952.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 40550244, 40550221, 40550223, 40550377 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/40550233/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/40550243/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/40550234/1/ammo.js', 'preload' : true},
+];
