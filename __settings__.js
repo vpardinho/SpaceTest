7a6587baaa1bcc87b51830ad32b584d5ee258bc5
victorparx <victorpardinho@gmail.com>
2021-01-03 13:47:07 +0100

    Add
    Add to github pages

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1058416.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 40259504, 40259506, 40312790, 40312793, 40317087, 40317204, 40317205, 40317206, 40326779, 40329439 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
